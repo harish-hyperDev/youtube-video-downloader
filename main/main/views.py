3f481068901c07447f4127a7dc0d6988b190874b
harish-hyperDev <coolprogrammer.777@gmail.com>
2023-04-03 17:57:14 +0530

    creating api with fastapi and wsgi

--- main/main/views.py
+++ main/main/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from starlette.responses import FileResponse
+
 from pytube import YouTube
+
+
+def send_file(file_path, file_name):
+    return FileResponse(
+        path = file_path,
+        media_type = 'application/octet-stream',
+        filename = file_name
+    )
 
 
 def home(request):
@@ -10,23 +20,49 @@ def home(request):
         video = request.POST["video-link"]
 
         youtube_object = YouTube(video)
-        #youtube_object = youtube_object.streams.get_highest_resolution()
-        youtube_object = youtube_object.streams.all()
-
-        try:
-            print("Downloading...")
-            # youtube_object.download()
-            print("Downloaded!")
-        except:
-            print("An error has occurred")
-
-        print("Download is completed successfully")
+        youtube_object = youtube_object.streams.filter(file_extension='mp4')
 
         filtered_object = []
         for k in youtube_object:
-            print("got -- ", k.video_codec)
-            foo = {"title": k.title, "type": }
-            filtered_object.append()
+
+            try:
+                # print("res ", k)
+
+                foo = {
+                    "title": k.title,
+                    "type": k.mime_type.split('/')[0],
+                    "full_type": k.mime_type.split('/')[0] + "/" + k.resolution + " - " + str(k.filesize_mb) + " MB",
+                    "format": k.mime_type.split('/')[1],
+                    "index": youtube_object.index(k),
+                    "full": k,
+                    "full_name": k.get_file_path().split('/')[len(k.get_file_path().split('/')) - 1],
+                    "full_path": k.get_file_path(),
+                }
+                filtered_object.append(foo)
+
+            except:
+                pass
+
+        try:
+            if request.POST["video-index"]:
+                selected_video = request.POST["video-index"]
+                print(selected_video)
+
+                for k in filtered_object:
+                    if k["full_type"] == selected_video:
+                        print("full name ", k["full_name"])
+                        print("Downloading...")
+                        k["full"].download()
+                        print("Downloaded!")
+                        # send_file(k["full_name"])
+                        # k["full"].download()
+
+                        send_file(file_path=k["full_path"], file_name=k["full_name"])
+                        print("file sent")
 
 
-        return render(request, "result.html", {"video": { "link": video, "result": youtube_object }})
+
+        except:
+            pass
+
+        return render(request, "result.html", {"video": {"link": video, "result": filtered_object}})
--- main/main/wsgi.py
+++ main/main/wsgi.py
@@ -8,9 +8,27 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+
+# New Imports
+
+from importlib.util import find_spec
+from fastapi import FastAPI
+from fastapi.middleware.wsgi import WSGIMiddleware
+from fastapi.staticfiles import StaticFiles
+from api import router
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 application = get_wsgi_application()
+
+app = FastAPI()
+app.mount("/admin", WSGIMiddleware(application))
+app.mount("/static",
+    StaticFiles(
+         directory=os.path.normpath(
+              os.path.join(find_spec("django.contrib.admin").origin, "..", "static")
+         )
+   ),
+   name="static",
+)
